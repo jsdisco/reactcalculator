a1c45eb5146ce383024730109200c40819aa5691
jsdisco <mar18yyzz@gmail.com>
2020-10-10 16:00:23 +0200

    replaced mathjs library with custom evaluation function (immediate execution logic)

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,30 +1,16 @@
 import React, {useState} from 'react';
 import Display from './Display.js';
 import Keypad from './Keypad.js';
-import { create, all } from "mathjs";
 
 function Calculator(){
 
-    const config = {
-          epsilon: 1e-12,
-          matrix: "Matrix",
-          number: "number",
-          precision: 64,
-          predictable: false,
-          randomSeed: null
-        };
-    const math = create(all, config);
-
-
     const [input, setInput] = useState(['0']);
-    const [currNum, setCurrNum] = useState('0');
+    const [currNum, setCurrNum] = useState('');
 
     function updateInput(e){
         if (e.target.classList.contains('clear')) {
             clear();
-            return;
-        };
-        if (e.target.classList.contains('number')){
+        } else if (e.target.classList.contains('number')){
             handleNumClick(e)
         } else if (e.target.classList.contains('operator')){
             handleOpClick(e)
@@ -33,94 +19,126 @@ function Calculator(){
 
     function clear(){
         setInput(['0']);
-        setCurrNum('0');
+        setCurrNum('')
     }
 
     function isFirstClick(){
         return input.length === 1 && input[0] === '0';
     }
 
-    function lastInputType(index){
+    function lastInputType(indexFromEnd) {
         let type;
-        if (!isNaN(input[input.length-1-index])) {
-            type = 'num'
-        } else if (input[input.length-1-index] === '.'){
-            type = 'decimal'
-        } else if (input[input.length-1-index] === '-') {
-            type = 'minus'
-        } else if (isNaN(input[input.length-1-index])){
-            type = 'other-op'
-        };
+        if (!isNaN(input[input.length - 1 - indexFromEnd])) {
+            type = "num";
+        } else if (input[input.length - 1 - indexFromEnd] === ".") {
+            type = "decimal";
+        } else if (input[input.length - 1 - indexFromEnd] === "-") {
+            type = "minus";
+        } else if (isNaN(input[input.length - 1 - indexFromEnd])) {
+            type = "other-op";
+        }
         return type;
     }
 
-
     function handleNumClick(e){
-        let newInput = [...input];
+        let newNum = currNum;
+        let newInputArr = [...input];
         let thisInput = e.target.textContent;
 
         if (thisInput === '.'){
             if (!currNum.includes('.')){
                 if (isFirstClick() || lastInputType(0) === 'num'){
-                    newInput.push(thisInput);
-                    setCurrNum(prev => prev.concat('0').concat(thisInput))
-                } else if (lastInputType(0) === 'other-op') {
-                    newInput.push('0');
-                    newInput.push(thisInput);
-                    setCurrNum(prev => prev.concat('0').concat(thisInput))
+                    newInputArr.push(thisInput);
+                    newNum = isFirstClick() ? newNum.concat('0.') : newNum.concat('.')
+                } else if (lastInputType(0) === 'minus' || lastInputType(0) === 'other-op') {
+                    newInputArr.push('0');
+                    newInputArr.push(thisInput);
+                    newNum = newNum.concat('0.')
                 }
             }
         } else {
             if (isFirstClick()){
-                newInput.pop();
+                newInputArr.pop();
             }
-            newInput.push(thisInput);
-            setCurrNum(prev => prev.concat(thisInput))
+            newInputArr.push(thisInput);
+            newNum = newNum.concat(thisInput);
         }
-        setInput(newInput);
+        setCurrNum(newNum);
+        setInput(newInputArr);
     }
 
     function handleOpClick(e){
-        let newInput = [...input];
+        let newInputArr = [...input];
         let thisInput = e.target.textContent;
 
         if (lastInputType(0) !== 'decimal'){
-            setCurrNum('0');
+            setCurrNum('');
         }
         if (thisInput === '-'){
             if (isFirstClick()) {
-                newInput.pop();
+                newInputArr.pop();
             }
             if (lastInputType(0) === 'num' || (lastInputType(1) === 'num' && (lastInputType(0) === 'minus' || lastInputType(0) === 'other-op'))){
-                newInput.push(thisInput)
+                newInputArr.push(thisInput)
             }
-            setInput(newInput);
         } else if (thisInput !== '=') {
             if (!isFirstClick()){
                 if (lastInputType(0) === 'num') {
-                    newInput.push(thisInput)
-                } else if (lastInputType(0) === 'other-op' || lastInputType(0) === 'minus'){
-                    if (lastInputType(1) === 'other-op' || lastInputType(1) === 'minus'){
-                        newInput.pop();
+                    newInputArr.push(thisInput)
+                } else if (lastInputType(0) === 'minus' || lastInputType(0) === 'other-op'){
+                    if (lastInputType(1) === 'minus' || lastInputType(1) === 'other-op'){
+                        newInputArr.pop();
                     }
-                    newInput.pop();
-                    newInput.push(thisInput)
+                    newInputArr.pop();
+                    newInputArr.push(thisInput)
                 }
-            };
-            setInput(newInput)
-        } else if (thisInput === '='){
-            setInput(newInput);
-            evaluate();
+            }
+        }
+        setInput(newInputArr);
+        if (thisInput === '='){
+            const isValid = lastInputType(0) === 'num';
+            if (isValid){
+                evaluate();
+            }
         }
     }
 
     function evaluate(){
-        const str = input.join('');
-        const result = math.evaluate(str);
+        let str = input.join('');
+        let numbersArr = str.split(/\+|-|\*|\//).filter(e => !!e);
+        let opsArr = str.split(/[0-9]|\./).filter(e => !!e).map(e => e === '+-' ? '-' : e === '--' ? '+' : e);
+
+        if (opsArr.length === numbersArr.length){
+            opsArr.shift();
+            numbersArr[0] = `-${numbersArr[0]}`
+        };
+
+        opsArr = opsArr.map((e,i) => {
+            if (e.length === 2){
+                numbersArr[i+1] = `-${numbersArr[i+1]}`;
+                return e.slice(0,1)
+            };
+            return e;
+        });
+
+        opsArr.unshift('+');
+        let result = numbersArr.reduce((acc,n,i) => {
+            let op = opsArr[i];
+            let num = Number(n);
+            switch(op){
+                case '+': acc += num; break;
+                case '-': acc -= num; break;
+                case '*': acc *= num; break;
+                case '/': acc /= num; break;
+                default: return acc;
+            };
+            return acc;
+        },0);
+
+        result = Math.round(1000000000000 * result) / 1000000000000;
         setInput([result]);
         setCurrNum(result.toString());
     }
-
 
     const display = input.join('');
 
